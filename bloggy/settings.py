8e63643c2b2b18354ce9363d2a3afe5aff51b3ad
rushaad29 <m.rushaadq@gmail.com>
2025-04-14 12:26:14 +0530

    production test

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_t-%)djxz4=hg*dr76)q4tt5f5djlmmvokibhbtv@owkd7rv7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
